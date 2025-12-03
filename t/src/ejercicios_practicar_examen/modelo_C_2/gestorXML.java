e5ef34fab69b7958cc1772cfedb2f2ff5da38b76
Pablo <pgalilea@arsys.es>
2025-12-03 12:55:30 +0100

    examen 3 medio hecho

--- t/src/ejercicios_practicar_examen/modelo_C_2/gestorXML.java
+++ t/src/ejercicios_practicar_examen/modelo_C_2/gestorXML.java
@@ -1,13 +1,19 @@
 package ejercicios_practicar_examen.modelo_C_2;
 
 import java.io.File;
-
+import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 public class gestorXML {
     private static String nombre  = "metadatos.xml";
@@ -19,6 +25,9 @@ public class gestorXML {
                 DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
                 Document doc = dbf.newDocumentBuilder().newDocument();
                 Element raiz = doc.createElement("ficheros");
+                doc.appendChild(raiz);
+                guardarDoom(doc);
+                
             }catch(IllegalAccessError iAE){
                 iAE.printStackTrace();
 
@@ -28,6 +37,45 @@ public class gestorXML {
             
             
         }
+    }
+    public void guardarDoom(Document doc){
+        try {
+            Transformer trans = TransformerFactory.newInstance().newTransformer();
+            trans.transform(new DOMSource(doc), new StreamResult(new File(nombre)) );
+
+        } catch (TransformerException e) {
+            e.printStackTrace();
+            
+        }
+    }
+    public void addFichero(String nom, int tam){
+        Document doc = leerDOM();
+        Element raiz = doc.getDocumentElement();
+        Element fichero = doc.createElement("fichero");
+        Element nombre = doc.createElement("nombre");
+        nombre.setTextContent(nom);
+        Element tama = doc.createElement("tama");
+        tama.setTextContent(String.valueOf(tam));
+        fichero.appendChild(nom)
+        raiz.appendChild(fichero);
+
+
+    }
+    public Document leerDOM(){
+        try{
+             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            return dbf.newDocumentBuilder().parse(new File(nombre));
+        }catch( IOException ex){
+            ex.printStackTrace();
+        }catch(SAXException e){
+            e.printStackTrace();
+        }catch(ParserConfigurationException pe){
+            pe.printStackTrace();
+        }
+        
+        };
+       
+        
     }
 
     
